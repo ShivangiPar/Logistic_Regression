8e5b1b901bd8a15cf8dfabb8eb9d30c669b176f9
Shivangi <shivangi.250193@gmail.com>
2023-12-19 16:21:04 -0800

    V1

---
+++ Logistic_Regression.py
@@ -0,0 +1,41 @@
+import pandas as pd
+import numpy as np
+
+
+def sigmoid(x):
+    u = 1 / (1 + np.exp(-x))
+    return u
+    
+
+
+
+class Logistic_Regression():
+
+    def __init__(self,learn_rate,no_iteration):
+        self.learn_rate = learn_rate
+        self.no_iteration = no_iteration
+        self.weight = None
+        self.bias = None
+
+    def fit(self,X,y):
+        sample, feature = X.shape
+        self.weight = np.zeros(feature)
+        self.bais = 0
+
+        for _ in range(self.no_iteration):
+            linear_func = np.dot(X,self.weight) + self.bais
+            predict = sigmoid(linear_func)
+
+            dw = (1/sample) * np.dot(X.T,predict - y)
+            db = (1/sample) * np.sum(predict - y)
+     
+
+            self.weight = self.weight - self.learn_rate*dw
+            self.bias = self.bais - self.learn_rate*db
+
+    def pred(self,X):
+        predict = np.dot(X,self.weight) + self.bais
+        y_predict = sigmoid(predict)
+        class_pred = [0 if y < 0.5 else 1 for y in y_predict]
+        return class_pred
+
